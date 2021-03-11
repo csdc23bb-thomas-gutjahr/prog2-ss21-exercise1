11de830548abaf11fadcfc67e3ca73ecbc11410a
csdc23bb-thomas-gutjahr <71706791+csdc23bb-thomas-gutjahr@users.noreply.github.com>
2021-03-11 20:00:48 +0100

    Wrote Test Cases and Methods for Extendends

--- src/main/java/CheckPassword.java
+++ src/main/java/CheckPassword.java
@@ -3,12 +3,12 @@ import java.util.regex.Pattern;
 public class CheckPassword {
 
 
-    public final Pattern textPatternLower = Pattern.compile("[a-z]");
-    public final Pattern textPatternUpper = Pattern.compile("[A-Z]");
-    public final Pattern textPatternDigit = Pattern.compile("[0-9]");
-    public final Pattern textPatternSpecial = Pattern.compile("[()#$?!%/@]");
-    public final Pattern textPatternTwoDigitRow = Pattern.compile("(012|123|234|345|456|567|678|789)");
-    public final Pattern textPatternThreeDigitRow = Pattern.compile("\\d(\\d)\\1{3}");
+    public final Pattern textPatternLower = Pattern.compile("[a-z]");   //Lower Case Pattern
+    public final Pattern textPatternUpper = Pattern.compile("[A-Z]");   //Upper Case Pattern
+    public final Pattern textPatternDigit = Pattern.compile("[0-9]");   //Digit Pattern
+    public final Pattern textPatternSpecial = Pattern.compile("[()#$?!%/@]");   //Special Character Pattern
+    public final Pattern textPatternTwoDigitRow = Pattern.compile("(012|123|234|345|456|567|678|789)"); //Two Digits in Row Pattern
+    public final Pattern textPatternThreeDigitRow = Pattern.compile("\\d(\\d)\\1{3}");  //Three of the same Digits in Row Pattern
 
     String password;
 
@@ -64,12 +64,13 @@ public class CheckPassword {
         return textPatternSpecial.matcher(this.password).find();
     }
 
-
+    //Method to Check if there are more than two digits in a row e.x. 123
     public boolean checkTwoNumbersInRow() {
 
         return textPatternTwoDigitRow.matcher(this.password).find();
     }
 
+    //Method to Check if there are more than three digits of the same in a row ex 1111
     public boolean checkThreeDigitRow() {
 
         return textPatternThreeDigitRow.matcher(this.password).find();
