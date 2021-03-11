400d6c3f7c3ddb471876cb809ca155dc344a72d9
csdc23bb-thomas-gutjahr <71706791+csdc23bb-thomas-gutjahr@users.noreply.github.com>
2021-03-11 17:23:20 +0100

    Created Methode for Checkdigit

--- src/main/java/CheckPassword.java
+++ src/main/java/CheckPassword.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 public class CheckPassword {
 
 
-    public final Pattern textPatternLower = Pattern.compile("(?=.*[a-z])");
+    public final Pattern textPatternLower = Pattern.compile("([a-z])");
     public final Pattern textPatternUpper = Pattern.compile("(?=.*[A-Z])");
     public final Pattern textPatternDigit = Pattern.compile("(?=.*[0-9])");
 
